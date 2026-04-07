013ea23f6e5090661bd024e90681a4340429f790
Gokulkannarasan <gokulkannarasan5@gmail.com>
2026-04-07 21:31:37 +0530

    this is the last dance

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
