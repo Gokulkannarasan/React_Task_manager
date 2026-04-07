55ae920e822861372c805e025ef48dbcfe866296
Gokulkannarasan <gokulkannarasan5@gmail.com>
2026-04-07 21:20:37 +0530

    changed the backed url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://react-task-manager-68qf.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
