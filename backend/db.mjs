e3b1b232fb609e1bbbd6c240a3bc66528ce29103
Gokulkannarasan <gokulkannarasan5@gmail.com>
2026-04-10 13:34:24 +0530

    Updated db.mjs

--- backend/db.mjs
+++ backend/db.mjs
@@ -25,16 +25,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,   // ✅ REQUIRED
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false   // ✅ REQUIRED for Railway
-    }
-});
+export const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
     if (err) {
